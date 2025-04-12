1a16bd26fea747503eae8e1e6c2aa4d22b4d2288
HOMEPC\anton <antonkuzan@gmail.com>
2025-04-12 18:54:42 +0300

    Project started

---
+++ LearningDefaultSln/LearningDefaultSln/Program.cs
@@ -0,0 +1,17 @@
+﻿//01:33:33
+using System;
+using System.Text;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            
+            
+        }
+    }
+}
