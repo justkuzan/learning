1b66e6be15d207deae6c2f3467ab8e847f4e0f01
HOMEPC\anton <antonkuzan@gmail.com>
2025-04-13 23:43:32 +0300

    13-04-2025

--- LearningDefaultSln/LearningDefaultSln/Program.cs
+++ LearningDefaultSln/LearningDefaultSln/Program.cs
@@ -1,4 +1,5 @@
-﻿//01:33:33
+﻿//02:39:18
+//while, когда мы не знаем четкие границы цикла. Когда мы точно можем посчитать кол-во шагов и размер цикла, то мы используем for
 using System;
 using System.Text;
 
@@ -10,8 +11,27 @@ namespace Homework
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
+
+            int rightPassword = 1234;
+            int triesCount = 3;
+            string userInput;
             
-            
+            for (int i = 0; i < triesCount; i++)
+            {
+                Console.WriteLine("Введите пароль");
+                userInput = Console.ReadLine();
+                
+                if (Convert.ToInt32(userInput) == rightPassword)
+                {
+                    Console.WriteLine("Добро пожаловать!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный пароль. У вас осталось {triesCount - (i+1)} попытки");
+                }
+            }
+            Console.WriteLine("Попытки закончились. Доступ заблокирован.");
         }
     }
 }
