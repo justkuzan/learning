34c4bd4e750513b0abce607eb14a412a812b266d
HOMEPC\anton <antonkuzan@gmail.com>
2025-07-28 15:42:36 +0300

    Project Started

--- LearningDefaultSln/LearningDefaultSln/Program.cs
+++ LearningDefaultSln/LearningDefaultSln/Program.cs
@@ -1,37 +1,14 @@
-﻿//02:39:18
-//while, когда мы не знаем четкие границы цикла. Когда мы точно можем посчитать кол-во шагов и размер цикла, то мы используем for
+﻿//04:13:00 с#
 using System;
-using System.Text;
-
-namespace Homework
+namespace LearningDefaultSln
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.InputEncoding = Encoding.UTF8;
-
-            int rightPassword = 1234;
-            int triesCount = 3;
-            string userInput;
-            
-            for (int i = 0; i < triesCount; i++)
-            {
-                Console.WriteLine("Введите пароль");
-                userInput = Console.ReadLine();
-                
-                if (Convert.ToInt32(userInput) == rightPassword)
-                {
-                    Console.WriteLine("Добро пожаловать!");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"Неверный пароль. У вас осталось {triesCount - (i+1)} попытки");
-                }
-            }
-            Console.WriteLine("Попытки закончились. Доступ заблокирован.");
+            Console.WriteLine("Введите размер массива: ");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Вы ввели {input}");
         }
     }
 }
