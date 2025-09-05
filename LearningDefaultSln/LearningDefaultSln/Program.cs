335ae576c871208b402b341f9240010140e52753
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-09-05 17:25:13 +0300

    Method for searching index

--- LearningDefaultSln/LearningDefaultSln/Program.cs
+++ LearningDefaultSln/LearningDefaultSln/Program.cs
@@ -1,4 +1,4 @@
-﻿//04:13:00 с#
+﻿//05:52:00 с#
 using System;
 namespace LearningDefaultSln
 {
@@ -6,9 +6,33 @@ namespace LearningDefaultSln
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите размер массива: ");
-            int input = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Вы ввели {input}");
+            Random randomArrayLength = new Random();
+            int[] array =  new int[randomArrayLength.Next(10, 50)];
+
+            Console.WriteLine("Массив: ");
+            
+            foreach (var i in array)
+            {
+                array[i] = randomArrayLength.Next(1,10);
+                Console.Write(array[i]+" ");
+            }
+
+            Console.WriteLine("\nИндекс какого числа вы ищите?");
+            int desiredNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i != desiredNumber ; i++)
+            {
+                if (i == array.Length)
+                {
+                    Console.WriteLine("Число не найдено");
+                }
+                else
+                {
+                    Console.WriteLine($"Индекс числа {desiredNumber}: {array[i]}");
+                }
+            }
+            
+            // Console.WriteLine($"Число {number} находится под индексом {desiredIndex}");
         }
     }
 }
