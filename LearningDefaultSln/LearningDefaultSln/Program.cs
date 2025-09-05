2f9a177e6f5f3c64d2562346f02872c2900c052c
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-09-05 18:00:31 +0300

    Method for searching index. My own corrected version (via for)

--- LearningDefaultSln/LearningDefaultSln/Program.cs
+++ LearningDefaultSln/LearningDefaultSln/Program.cs
@@ -10,29 +10,31 @@ namespace LearningDefaultSln
             int[] array =  new int[randomArrayLength.Next(10, 50)];
 
             Console.WriteLine("Массив: ");
-            
-            foreach (var i in array)
+
+            for (int i = 0; i < array.Length; i++)
             {
                 array[i] = randomArrayLength.Next(1,10);
                 Console.Write(array[i]+" ");
             }
-
+            
             Console.WriteLine("\nИндекс какого числа вы ищите?");
             int desiredNumber = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i != desiredNumber ; i++)
+            for (int i = 0; i < array.Length ; i++)
             {
-                if (i == array.Length)
+                if (array[i] == desiredNumber)
+                {
+                    Console.WriteLine($"Индекс числа {desiredNumber}: {i}");
+                    break;
+                }
+                
+                else if (desiredNumber != array[i])
                 {
                     Console.WriteLine("Число не найдено");
-                }
-                else
-                {
-                    Console.WriteLine($"Индекс числа {desiredNumber}: {array[i]}");
+                    break;
                 }
             }
             
-            // Console.WriteLine($"Число {number} находится под индексом {desiredIndex}");
         }
     }
 }
